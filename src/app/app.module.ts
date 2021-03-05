df4318459ca13b2bfb1b8cd6ae0f638c0787593e
Mihir Jain <mihir@chingari.io>
2021-03-05 19:54:55 +0530

    added changes for login and signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthService } from './services/auth/auth.service';
 import { HomepageComponent } from './homepage/homepage.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from '../app/auth/login/login.component';
+import { SignupComponent } from '../app/auth/signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -16,9 +20,12 @@ import { SignupComponent } from './signup/signup.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

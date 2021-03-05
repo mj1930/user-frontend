de9f5bd67284555ee1d6a803d0c0c9d7596d1073
chandan <chandansinghkanyal@gmail.com>
2021-03-05 17:54:14 +0530

    homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { UserHomepageComponent } from './user-homepage/user-homepage.component';
 
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: '/'},
+  {path: 'home', component: UserHomepageComponent},
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { UserHomepageComponent } from './user-homepage/user-homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    UserHomepageComponent
   ],
   imports: [
     BrowserModule,

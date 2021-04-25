79dc834172ad750f81b12fd6a0326ee2de80c3e2
Chandan Kanyal <chandansinghkanyal@gmail.com>
2021-04-25 18:04:07 +0530

    cxdsx

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://b9c465594ba8.ngrok.io/',
+  apiUrl: 'http://ae1f4b53c41b.ngrok.io',
   sellerUrl: 'http://localhost:3003/',
   adminApiUrl: 'http://localhost:3002/'
 };

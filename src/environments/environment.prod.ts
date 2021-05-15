6bb08aa16507fcccc60ed4c62196410eb018a66c
Chandan Kanyal <chandansinghkanyal@gmail.com>
2021-05-15 15:54:32 +0530

    map api urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://13.233.168.72:3001/',
-  sellerUrl: 'http://65.2.73.90:3003/',
-  adminApiUrl: 'http://65.2.31.91:3002/'
+  apiUrl: 'https://6arhtir1m7.execute-api.ap-south-1.amazonaws.com/',
+  sellerUrl: 'https://abrwb40304.execute-api.ap-south-1.amazonaws.com/',
+  adminApiUrl: 'https://rgr3szfd44.execute-api.ap-south-1.amazonaws.com/'
 };

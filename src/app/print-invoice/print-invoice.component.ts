91484db529262d5e2a10bb98ab916a5cc4441cff
Chandan Kanyal <chandansinghkanyal@gmail.com>
2021-06-01 18:37:28 +0530

    Homepage Slideshow

--- src/app/print-invoice/print-invoice.component.ts
+++ src/app/print-invoice/print-invoice.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import * as $ from 'jquery';
-import {jsPDF} from 'jspdf';
 import { AuthService } from '../services/auth/auth.service';
 
 @Component({
@@ -21,22 +19,6 @@ export class PrintInvoiceComponent implements OnInit {
   ngOnInit(): void {
     this.orderId = this.router.snapshot.url[1].path;
     this.getInvoiceDetails();
-    $(document).ready(function(){
-      var doc = new jsPDF();
-      var specialElementHandlers = {
-        '#editor': function (element, renderer) {
-          return true;
-        }
-      };
-
-      $('.downloadPDF').on('click', function () {
-        // doc.fromHTML($('#content').html(), 15, 15, {
-        //   'width': 170,
-        //   'elementHandlers': specialElementHandlers
-        // });
-        // doc.save('sample-file.pdf');
-      });
-    });
   }
 
   getInvoiceDetails() {
